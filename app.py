09862114143c02e6f9f46be1eec0d0282e50895e
JKamlah <jan.kamlah@gmail.com>
2021-08-17 17:31:40 +0200

    Functionalize the code, add a NameRegister class and add pickle to load faster.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import random
-import datetime
-import time
+import timeit
 import openpyxl as xl
 from webscrape import webscraping_last_names as get_last_names, webscraping_first_names as get_first_names
 from pathlib import Path
+import sys
+from dataclasses import dataclass
 
 
 def rng_intro():
@@ -16,76 +17,21 @@ Last names from:  https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Namen
     """)
 
 
-def convert_timedelta(duration):
-    seconds = duration.seconds
-    minutes = (seconds % 3600) // 60
-    seconds = (seconds % 60)
-    return minutes, seconds
-
-
-def stats(wb_filename):
-    wb = xl.load_workbook(wb_filename)
-    sheet = wb['sheet1']
-
-    for column in range(1, sheet.max_column + 1):
-        compare_row = sheet.min_row
-        for row in range(1, sheet.max_row + 1):
-            cell = sheet.cell(row, column)
-            if cell.value is not None and column == 1:
-                if row >= compare_row:
-                    stats.max_row_firstname = row
-                else:
-                    break
-            elif cell.value is not None and column == 2:
-                if row >= compare_row:
-                    stats.max_row_lastname = row
-                else:
-                    break
-
-
-def create_random_name(wb_filename, xcount):
-    wb = xl.load_workbook(wb_filename)
-    sheet = wb['sheet1']
-
-    # Get max non empty cell numbers for columns 1 - 2
-    for column in range(1, sheet.max_column + 1):
-        compare_row = sheet.min_row
-        for row in range(1, sheet.max_row + 1):
-            cell = sheet.cell(row, column)
-            if cell.value is not None and column == 1:
-                if row >= compare_row:
-                    stats.max_row_firstname = row
-                else:
-                    break
-            elif cell.value is not None and column == 2:
-                if row >= compare_row:
-                    stats.max_row_lastname = row
-                else:
-                    break
-
-    # Get random cell numbers for column 1 - 2
-    dice_first_name = random.randint(1, stats.max_row_firstname)
-    dice_second_name = random.randint(1, stats.max_row_firstname)
-    dice_last_name = random.randint(1, stats.max_row_lastname)
-
-    # Set random cells for column 1 - 2
-    cell_first_name = sheet.cell(dice_first_name, 1)
-    cell_second_name = sheet.cell(dice_second_name, 1)
-    cell_last_name = sheet.cell(dice_last_name, 2)
-
-    print(f'{cell_first_name.value} {cell_second_name.value} {cell_last_name.value}')
-
-
-rng_intro()
-
-is_input_valid = False
-while is_input_valid is False:
+def select_database():
     result = input("🦥 [C]reate or [W]ork with existing database? [Q] to stop slothing... ").capitalize()
     if result == 'C':
-        is_input_valid = True
         filename = input('🦥 Name your database: ')
         wb_filename = f'{filename}.xlsx'
-
+        if Path(wb_filename).exists():
+            print('Database already exists.')
+            if input("🦥 Do you want to overwrite it? [Y/n] ").capitalize() in ('', 'Y', 'Yes'):
+                Path(wb_filename).unlink()
+                if Path(wb_filename.replace('.xlsx', '.pkl')).exists():
+                    Path(wb_filename.replace('.xlsx', '.pkl')).unlink()
+            elif input("🦥 Do you want to use it? [Y/n] ").capitalize() in ('', 'Y', 'Yes'):
+                return wb_filename
+            else:
+                return None
         wb = xl.Workbook()
         sheet = wb.active
         sheet.title = "sheet1"
@@ -93,7 +39,7 @@ while is_input_valid is False:
 
         print('SlothGen slowly started building your database ... 🦥')
 
-        start_log = datetime.datetime.now()
+        start_timer = timeit.default_timer()
         # Scrape first names (female + male) from vornamen.com
         get_first_names.web_scrape_first_names(wb_filename)
         # Scrape last names (German) from wikipedia.de
@@ -101,56 +47,84 @@ while is_input_valid is False:
         # Scrape last names from familyeducation.com
         get_last_names.scraping_last_names_familyeducationdotcom(wb_filename)
 
-        end_log = datetime.datetime.now()
-        duration = end_log - start_log
-        minutes, seconds = convert_timedelta(duration)
-        print('\nSlothGen 🦥 successfully created your database in {} minutes and {} seconds.'.format(minutes, seconds))
+        duration_seconds = int(timeit.default_timer() - start_timer)
+        print(f'\nSlothGen 🦥 successfully created your database in {duration_seconds//60} minutes and {duration_seconds%60} seconds.')
     elif result == 'W':
-        is_input_valid = True
-        is_filename_valid = False
-        while is_filename_valid is False:
-            wb_filename = input('Type the name of your database with file extension (example: database.xlsx): ')
-            if Path(wb_filename).is_file():
-                is_filename_valid = True
-            else:
-                is_filename_valid = False
-                print('Sorry. No file of that name exists.\n')
+        wb_filename = input('Type the name of your database with file extension (example: database.xlsx): ')
+        if not Path(wb_filename).is_file():
+            print('Sorry. No file of that name exists.\n')
+            return None
     elif result == 'Q':
-        exit()
+        sys.exit(0)
     else:
-        is_input_valid = False
         print('Please choose [C]reate or [W]ork.')
+        return None
+    return wb_filename
 
-stats(wb_filename)
-print(f'\nSlothGen 🦥 found {stats.max_row_firstname} first names and {stats.max_row_lastname} last names in your database.')
+@dataclass
+class NameRegister:
 
-get_more_names = True
-is_roll_dice_input_valid = False
+    def __init__(self, wb_filename):
+        self.wb_filename = wb_filename
+        self._read_names(wb_filename)
 
-while get_more_names is True:
+    def _read_names(self, wb_filename):
+        wb = xl.load_workbook(wb_filename)
+        sheet = wb['sheet1']
+        self.first_names = list(set([name.value for name in sheet['A']]))
+        self.last_names = list(set([name.value for name in sheet['B']]))
+
+    def create_random_name(self):
+        print(f'{random.choice(self.first_names)} {random.choice(self.first_names)} {random.choice(self.last_names)}')
+
+    def pickle(self):
+        import pickle
+        with open(self.wb_filename.replace('.xlsx', '.pkl'), "wb") as fout:
+            pickle.dump(self, fout)
+
+def create_random_names(name_register):
     roll_dice = input("\n🦥 Generate [S]ingle name, [N]umber of names or [Q]uit? ").capitalize()
     if roll_dice == 'S':
-        xcount = 1
-        is_roll_dice_input_valid = True
-        create_random_name(wb_filename, xcount)
+        name_register.create_random_name()
     elif roll_dice == 'Q':
-        is_roll_dice_input_valid = True
-        get_more_names = False
+        sys.exit(0)
     elif roll_dice == 'N':
-        start = time.time()
-        is_roll_dice_input_valid = True
-        is_xcount_corret = False
-        while is_xcount_corret is False:
+        while True:
             try:
                 xcount = int(input('\n🦥 Generate how many names? '))
-                is_xcount_corret = True
-                print('Started slothing ... 🦥')
-                for x in range(1, xcount + 1):
-                    create_random_name(wb_filename, xcount)
-            except ValueError:
-                print('\nPlease enter a valid number.')
-        duration = time.time() - start
-        print('\n(SlothGen 🦥 took %.0f seconds to generate your names.)' % duration)
+                if xcount > 0:
+                    break
+            except:
+                print("That's not a valid option!")
+        start_timer = timeit.default_timer()
+        print('Started slothing ... 🦥')
+        for x in range(0, xcount):
+            name_register.create_random_name()
+        duration_seconds = int(timeit.default_timer() - start_timer)
+        print(f'\n(SlothGen 🦥 took {duration_seconds} seconds to generate your names.)')
     else:
-        is_roll_dice_input_valid = False
         print('Please press [N] or [Q].')
+
+
+def main():
+    rng_intro()
+
+    wb_filename = None
+    while wb_filename is None:
+        wb_filename = select_database()
+
+    if Path(wb_filename.replace('.xlsx', '.pkl')).exists():
+        import pickle
+        with open(wb_filename.replace('.xlsx', '.pkl'), "rb") as fin:
+            name_register = pickle.load(fin)
+    else:
+        name_register = NameRegister(wb_filename)
+        name_register.pickle()
+
+    print(f'\nSlothGen 🦥 found {len(name_register.first_names)} first names and {len(name_register.last_names)} last names in your database.')
+
+    while True:
+        create_random_names(name_register)
+
+if __name__ == '__main__':
+    main()
